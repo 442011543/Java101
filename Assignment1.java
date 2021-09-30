af97182d5ac2b9daf4ea898a1666e6a4a30a5e21
elyas <elyasmohammed78@gmail.com>
2021-09-30 11:41:08 +0300

    fix 2 errors

--- Assignment1.java
+++ Assignment1.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 //great job, thanks for your hard work
 //your grade is 4.5/5
 //@ change the class name from Main to match the file name Assignment1 ( golden rule )
-public class Main{
+public class Assignment1{
 public static void main(String[] args) {
     
     Scanner myScanner = new Scanner(System.in);
-    Float var1,var2;
+    float var1,var2;
     System.out.print("Enter Float Numb 1 : ");
     var1 = myScanner.nextFloat();
     System.out.print("Enter Float Numb 2 : ");
