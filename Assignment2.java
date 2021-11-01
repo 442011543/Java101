2a13b608c3175bc84155a6d16e20edd38c1ce027
442011543 <91121354+442011543@users.noreply.github.com>
2021-11-02 00:46:19 +0300

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class rock {
+public class Assignment2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String computerPlays = "rps";
