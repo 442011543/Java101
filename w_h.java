fd70725aa8d885c09b5bfd9789df47655017819e
442011543 <91121354+442011543@users.noreply.github.com>
2021-12-16 10:27:37 +0300

    Create w_h.java

---
+++ w_h.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class w_h {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        float h1,w1;
+
+        System.out.println("Enter high : ");
+        h1 = myScanner.nextFloat();
+        System.out.println("Enter witdth : ");
+        w1 = myScanner.nextFloat();
+
+        System.out.println( h1 * w1 );
+
+
+
+
+
+    }
+}
