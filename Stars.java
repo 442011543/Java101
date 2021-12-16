414ddd8e224125a0cef975931c8203d57f26cb9f
442011543 <91121354+442011543@users.noreply.github.com>
2021-12-16 10:23:43 +0300

    Create Stars.java

---
+++ Stars.java
@@ -0,0 +1,16 @@
+public class Stars {
+    public static void main(String[] args) {
+        int p = 5;
+        for(int index = 0; index < p; index++){
+            for (int index2 =0; index2 <= index; index2++){
+                System.out.print("* ");
+            }System.out.println(" ");
+
+        }
+
+    }
+}
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
