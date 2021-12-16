7150659fcb483845a8f6c7f1e01770b5681c4f54
442011543 <91121354+442011543@users.noreply.github.com>
2021-12-16 10:25:15 +0300

    Create p_n.java

---
+++ p_n.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class p_n {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        float h1;
+
+        System.out.println("Enter Number : ");
+        h1 = myScanner.nextFloat();
+
+        if(h1<0) {
+            System.out.println("Negative");
+        }
+        else{System.out.println("Postive");
+        }
+
+
+
+
+
+
+
+    }
+}
