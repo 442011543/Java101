1ae717241e4fbc3a4689d41bf10d9637e8b4c244
442011543 <91121354+442011543@users.noreply.github.com>
2021-12-16 10:33:52 +0300

    Update and rename p_n.java to PossitveAndNegative.java

--- PossitveAndNegative.java
+++ PossitveAndNegative.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class p_n {
+public class PossitveAndNegative {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         float h1;
