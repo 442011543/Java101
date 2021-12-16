6ec0a4994759dad1d1626fbf5f52d739af4a5b1c
442011543 <91121354+442011543@users.noreply.github.com>
2021-12-16 10:26:54 +0300

    Create subtractTwoNumberFunction.java

---
+++ subtractTwoNumberFunction.java
@@ -0,0 +1,12 @@
+public class subtractTwoNumberFunction {
+
+    static float sub(float x , float y ) {
+        return x - y ;
+
+    }
+ 
+    public static void main(String[] args) {
+        System.out.println(sub(5,2));
+
+}
+}
