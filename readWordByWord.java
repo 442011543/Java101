d0c0669954ae0e7c54c98f44814836d577943c31
442011543 <91121354+442011543@users.noreply.github.com>
2021-12-16 10:25:53 +0300

    Create readWordByWord.java

---
+++ readWordByWord.java
@@ -0,0 +1,14 @@
+public class readWordByWord {
+    public static void main(String[] args){
+        String name = "Elyas Mohammed Fatani";
+        for(int index = 0; index < name.charAt(index); index++){
+            if(name.charAt(index) == ' '){
+                System.out.println(" ");
+            }else{
+                System.out.print(name.charAt(index));
+            }
+
+        }
+
+    }
+}
