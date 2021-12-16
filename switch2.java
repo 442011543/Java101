2623b2d5e3c514f083eae67a1eb2f276d7387276
442011543 <91121354+442011543@users.noreply.github.com>
2021-12-16 10:27:19 +0300

    Create switch2.java

---
+++ switch2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class switch2 {
+    public static void main(String[] args) {
+        Scanner enter = new Scanner(System.in);
+        int numb;
+        float var1,var2;
+        System.out.print("Enter a Number 1 or 2 : ");
+        numb = enter.nextInt();
+        switch (numb) {
+            case 1:
+                System.out.print("Enter a Radius : ");
+                var1 = enter.nextFloat();
+                System.out.print("Sircle size = "+ (var1 * var1 * 3.14));
+                break;
+            case 2:
+                System.out.print("Enter a height : ");
+                var1 = enter.nextFloat();
+                System.out.print("Enter a width : ");
+                var2 = enter.nextFloat();
+                System.out.println("Area size = " + (var1 * var2));
+                break;
+            default:
+                System.out.print("Error !! enter number 1 or 2 ");
+
+
+        }
+
+
+    }
+}
