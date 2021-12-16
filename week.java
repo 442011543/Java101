7b57bcec06f1e2c0a1fef8d913953c5877208279
442011543 <91121354+442011543@users.noreply.github.com>
2021-12-16 10:27:53 +0300

    Create week.java

---
+++ week.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class week {
+    public static void main(String[] args) {
+        Scanner enter = new Scanner(System.in);
+        int numb;
+        System.out.print("Enter a number 1-7 : ");
+        numb = enter.nextInt();
+
+        switch (numb) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Week days");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Week end");
+                break;
+            default:
+                System.out.println("error");
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
