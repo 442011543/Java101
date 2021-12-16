0189fa0e07be0ab71452c5b0e1b5fe53a1252d6a
442011543 <91121354+442011543@users.noreply.github.com>
2021-12-16 10:24:10 +0300

    Create age.java

---
+++ age.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class age {
+    public static void main(String[] args){
+
+        Scanner add = new Scanner(System.in);
+        int var1;
+
+        System.out.print("Enter Your Birth Day : ");
+        var1 = add.nextInt();
+
+        System.out.println("Your age ="+ (2021 - var1));
+
+
+
+
+
+
+    }
+
+}
